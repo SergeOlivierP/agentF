b9a15da9ed63a708076e121694258545f3aa9e37
serge <serge-olivier.paquette@delvelabs.ca>
2018-01-20 14:49:21 -0500

    renamed simulation as session to better reflect purpose

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 from policy_keras import Policy
 from agent import Agent
-from simulation import Simulation
+from session import Session
 from market import Market
 from datetime import datetime
 import numpy as np
@@ -13,7 +13,7 @@ learning_rate = 1e-4
 gamma = 0.99  # discount factor for reward
 decay_rate = 0.99  # decay factor for RMSProp leaky sum of grad^2
 
-num_iterations = 5
+num_iterations = 5000
 market = Market('IntelDataSet.csv')
 D = np.shape(market.indices)[1]+2
 policy = Policy(H, D, gamma, batch_size, decay_rate, learning_rate)
@@ -25,7 +25,7 @@ running_reward = []
 for j in range(num_iterations):
 
     agent = Agent(c=5000, q=0)
-    sim = Simulation(agent, policy, market)
+    sim = Session(agent, policy, market)
     policy, assets = sim.run()
 
     running_reward.append(assets)
