389b9ea0c643124383056a08a2fcf9dc08d632a3
Thierry Claveau <thierry.claveau@ipsolcapital.com>
2018-02-25 17:45:48 -0500

    added cash asset in market

--- market.py
+++ market.py
@@ -4,12 +4,22 @@ import numpy as np
 
 class Market:
 
-    def __init__(self, file):
+    def __init__(self, file, number_of_assets=1):
         self.rawData = pd.read_csv(file)
-        self.asset_prices = self.rawData.IntelPriceUSD.values
+        self.number_of_assets = number_of_assets
+        # Compute number of signals. Remove 1 to account for the date column (first column)
+        self.number_of_signals = len(self.rawData.values[0,:]) - number_of_assets - 1 
+        self.duration = len(self.rawData.values[:,1])
 
         self.signals = self.rawData.iloc[:, 2:].values
-        self.signals -= np.mean(self.signals, 0)
-        self.signals /= np.std(self.signals, 0)
+        self.signals = self.preprocess_data(self.signals)
 
-        self.duration = len(self.rawData.IntelPriceUSD)
+        # temporarily force coded the cash asset
+        # self.asset_prices = self.rawData.IntelPriceUSD.values
+        self.asset_prices = np.append(np.ones([self.duration,1]), self.rawData.values[:,1:number_of_assets+1].reshape(self.duration,number_of_assets))
+
+    def preprocess_data(signals) # input := np.array
+        # thats where we should modify the signals, e.g. standardization
+        signals -= np.mean(signals, 0)
+        signals /= np.std(signals, 0)
+        pass
