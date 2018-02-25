2a89febc7616fc4b583259b2382d54716e5aaf8b
Thierry Claveau <thierry.claveau@ipsolcapital.com>
2018-02-25 13:22:35 -0500

    new variable names in market

--- market.py
+++ market.py
@@ -6,10 +6,10 @@ class Market:
 
     def __init__(self, file):
         self.rawData = pd.read_csv(file)
-        self.stock_price = self.rawData.IntelPriceUSD.values
+        self.asset_prices = self.rawData.IntelPriceUSD.values
 
-        self.indices = self.rawData.iloc[:, 2:].values
-        self.indices -= np.mean(self.indices, 0)
-        self.indices /= np.std(self.indices, 0)
+        self.signals = self.rawData.iloc[:, 2:].values
+        self.signals -= np.mean(self.signals, 0)
+        self.signals /= np.std(self.signals, 0)
 
         self.duration = len(self.rawData.IntelPriceUSD)
