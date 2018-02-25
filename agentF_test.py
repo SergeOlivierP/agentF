6373863f860e8727df2bcfbfd32b5c494945bf20
serge <serge-olivier.paquette@delvelabs.ca>
2018-02-25 15:33:46 -0500

    portfolio updates correctly

--- agentF_test.py
+++ agentF_test.py
@@ -1,13 +1,51 @@
 import unittest
 from unittest.mock import MagicMock
 from portfolio import Portfolio
-from environment import Environment
-from agent import Agent
+# from environment import Environment
+# from agent import Agent
 import numpy as np
 
 
 class PortfolioTest(unittest.TestCase):
 
+    def setUp(self):
+        self.money = 10
+        self.assets = 5
+        self.pf = Portfolio(cash=self.money, market_size=self.assets)
+
+    def test_portoflio_init(self):
+
+        self.assertEqual(self.pf.weights.shape, (1, self.assets))
+        self.assertEqual(self.pf.weights[0][0], 1)
+        self.assertEqual(self.pf.weights.shape, self.pf.quantities.shape)
+        self.assertEqual(self.pf.quantities[0][0], self.money)
+
+    def test_portfolio_update_respect_shape(self):
+        target = np.array([[0.20, 0.20, 0.20, 0.20, 0.20]])
+        prices = np.array([[1, 1, 1, 1, 1]])
+        prev_shape = self.pf.weights.shape
+        self.pf.update_transaction(target, prices)
+
+        self.assertEqual(self.pf.weights.shape, (prev_shape[0]+1, prev_shape[1]))
+
+    def test_portfolio_update_respect_weights(self):
+        target = np.array([[0.20, 0.20, 0.20, 0.20, 0.20]])
+        prices = np.array([[1, 1, 1, 1, 1]])
+        self.pf.update_transaction(target, prices)
+        self.assertEqual(np.sum(self.pf.weights[-1][:]), 1)
+
+    def test_portfolio_update_respect_quantities(self):
+        target = np.array([[0.20, 0.20, 0.20, 0.20, 0.20]])
+        prices = np.array([[1, 1, 1, 1, 1]])
+        self.pf.update_transaction(target, prices)
+        self.assertEqual(self.pf.quantities[-1][1], self.pf.quantities[-1][2])
+
+    def test_portfolio_update_works(self):
+        pf = Portfolio(cash=10, market_size=3)
+        target = np.array([[0.2, 0.4, 0.4]])
+        prices = np.array([[1, 2, 3]])
+        pf.update_transaction(target, prices)
+        self.assertTrue((pf.quantities == np.array([[10, 0, 0], [3, 2, 1]])).all())
 
 
 # class EnvironmentTest(unittest.TestCase):
@@ -21,7 +59,7 @@ class PortfolioTest(unittest.TestCase):
 
 
 
-# 
+#
 # class AgentTest(unittest.TestCase):
 #
 #     def setUp(self):
--- portfolio.py
+++ portfolio.py
@@ -3,17 +3,42 @@ import numpy as np
 
 class Portfolio:
 
-    def __init__(self, init_cash, market_size):
+    def __init__(self, cash, market_size):
         # (T,N) matrices where t in [0,T] is the day and n in [0,N] is the asset.
         self.weights = np.zeros((1, market_size))
-        self.weights[0] = 1
-        self.quantity = np.zeros((1, market_size))
-        self.quantity[0] = init_cash
+        self.weights[0][0] = 1
+        self.quantities = np.zeros((1, market_size))
+        self.quantities[0][0] = cash
 
         # (T,m) index to compute sharpe ratio in the reward function
         self.sharpe_cache = None
 
-    def adjust_quantities(target, prices):
-        # append a new row to the weights and quantity as close as the target as possible,
+    def update_transaction(self, target, prices):
+        # append a new row to the weights and quantities as close as the target as possible,
         # adjusted by the transaction cost.
-        pass
+
+        # Compute total value of assets
+        total_budget = np.sum(self.quantities[-1][:]*prices)
+
+        # Compute weights drift, delta and transaction cost
+        drifted_weights = self.quantities[-1][:]*prices/total_budget
+        delta_weights = target-drifted_weights
+
+        # Compute amount of money worth of each stock this delta allows
+        assets_budget = delta_weights*total_budget
+
+        # Round quantities to the floor integer according to allocated money
+        delta_quantities = np.floor(assets_budget/prices)
+
+        # adjust quantities accordingly, transaction costs will be here
+        new_quantities = self.quantities+delta_quantities
+        self.quantities = np.append(self.quantities, new_quantities, axis=0)
+
+        # sum unalocated money
+        unalocated_money = total_budget-np.sum(new_quantities*prices)
+        self.quantities[-1][0] = self.quantities[-1][0] + unalocated_money
+
+        # Compute new real weights
+        assets_values = self.quantities[-1][:]*prices
+        new_weights = assets_values/np.sum(assets_values)
+        self.weights = np.append(self.weights, new_weights, axis=0)
