1fdc38a4ef6f47d62ddb4847907af6b8f992c6c8
serge <serge-olivier.paquette@delvelabs.ca>
2018-03-01 15:04:46 -0500

    minor refactoring for code standards

--- market.py
+++ market.py
@@ -8,17 +8,20 @@ class Market:
         self.rawData = pd.read_csv(file)
         self.number_of_assets = number_of_assets
         # Compute number of signals. Remove 1 to account for the date column (first column)
-        self.number_of_signals = len(self.rawData.values[0,:]) - number_of_assets - 1 
-        self.duration = len(self.rawData.values[:,1])
+        self.number_of_signals = len(self.rawData.values[0, :]) - number_of_assets - 1
+        self.duration = len(self.rawData.values[:, 1])
 
         self.signals = self.rawData.iloc[:, 2:].values
         self.signals = self.preprocess_data(self.signals)
 
         # temporarily force coded the cash asset
         # self.asset_prices = self.rawData.IntelPriceUSD.values
-        self.asset_prices = np.append(np.ones([self.duration,1]), self.rawData.values[:,1:number_of_assets+1].reshape(self.duration,number_of_assets))
+        self.asset_prices = np.append(np.ones([self.duration, 1]),
+                                      self.rawData.values[:, 1:number_of_assets+1].reshape(self.duration,
+                                                                                           number_of_assets))
 
-    def preprocess_data(signals) # input := np.array
+    def preprocess_data(signals):
+        # input := np.array
         # thats where we should modify the signals, e.g. standardization
         signals -= np.mean(signals, 0)
         signals /= np.std(signals, 0)
